a65ffb74adb05c5d317fb5b3412a5a2f06644e52
sreelatha1104 <latha11211@gmail.com>
2016-04-28 12:32:09 -0500

    restaurant

---
+++ js/bootstrap-datepicker.js
@@ -0,0 +1,11 @@
+ <script type="text/javascript">
+ alert();
+             $(function () {
+                $('#datetimepicker1').datetimepicker();
+            });
+			
+            
+
+			
+			
+        </script>
